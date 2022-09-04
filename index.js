958aa1bc677b4075a9adbdf796fed49a74e19982
Tauhid8k <tauhid8k@gmail.com>
2022-09-04 19:59:06 +0600

    api & mongoDB setup

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/ProductModel');
+const app = express();
+
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected ${connect.connection.host}`);
+  } catch (error) {
+    console.error(`Error ${error.message}`);
+    process.exit(1);
+  }
+};
+connectDB();
+
+// Products Route
+app.get('/products', async function (req, res) {
+  const products = await Product.find();
+  console.log(products);
+  if (products) {
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error('No products found');
+  }
+});
+
+// Single Product Route
+app.get('/products/:id', async function (req, res) {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+app.listen(process.env.PORT || 3000);
